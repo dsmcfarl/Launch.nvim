d5a6d534d0f27de1df06cc1f88493fbc52c080f7
Dan McFarlane <dsmcfarl@gmail.com>
2023-11-04 12:45:59 -0500

    add python ftplugin

---
+++ ftplugin/python.lua
@@ -0,0 +1,18 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+keymap("n", "<leader>r", ":w<CR>:!python %<CR>", opts)
+vim.opt.textwidth = 79
