4894feb1ca7a3cd89329d6062da0e1a2154dcbf3
Dan McFarlane <dsmcfarl@gmail.com>
2023-12-28 20:26:59 -0600

    add ftplugin for rust

---
+++ ftplugin/rust.lua
@@ -0,0 +1,19 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+keymap("n", "<leader>r", ":RustRun<cr>", opts)
+-- vim.opt.textwidth = 99
+-- vim.opt.smartindent = false
